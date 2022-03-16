cb9cad77486c5ab6676095ccec8f12546adbf136
Maksim Kozhevnikov <mkozhevnikov23@yandex.ru>
2022-03-16 11:16:46 +0400

    Создать файл constant.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,72 +1,25 @@
-"use strict";
+import {
+  popups,
+  editBtn,
+  popupProfile,
+  addPlaceBtn,
+  popupPlace,
+  popupPlaceSubmitBtn,
+  userName,
+  userProfession,
+  popupName,
+  popupProfession,
+  popupPlaceName,
+  popupPlaceLink,
+  formEditUser,
+  formAddPlace,
+  places,
+  initialCards,
+  settings
+} from "../utils/constants.js"
 import { Card } from "../components/Card.js";
 import { FormValidator } from "../components/FormValidator.js";
 
-// All popups
-const popups = document.querySelectorAll('.popup');
-
-// Open and close pop-up Edit Profile
-const editBtn = document.querySelector('.profile__edit-bth');
-const popupProfile = document.querySelector('.popup_action_edit-profile');
-
-// Open and close pop-up Add Place
-const addPlaceBtn = document.querySelector('.profile__add-btn');
-const popupPlace = document.querySelector('.popup_action_add-place');
-const popupPlaceSubmitBtn = popupPlace.querySelector('.popup__save-btn');
-
-// Get info about user
-const userName = document.querySelector('.profile__name');
-const userProfession = document.querySelector('.profile__profession');
-const popupName = popupProfile.querySelector('.popup__input_data_name');
-const popupProfession = popupProfile.querySelector('.popup__input_data_job');
-
-// Get info about place
-const popupPlaceName = popupPlace.querySelector('.popup__input_data_place-name');
-const popupPlaceLink = popupPlace.querySelector('.popup__input_data_place-link');
-
-// Save new user info
-const formEditUser = popupProfile.querySelector('.popup__form');
-
-// Save new place
-const formAddPlace = popupPlace.querySelector('.popup__form');
-
-// Places
-const places = document.querySelector('.elements');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const settings = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__save-btn',
-  inactiveButtonClass: 'popup__save-btn_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-};
 
 // Validation
 const validateFormAddPlace = new FormValidator(settings, formAddPlace);
---
+++ scripts/utils/constants.js
@@ -0,0 +1,65 @@
+// All popups
+export const popups = document.querySelectorAll('.popup');
+
+// Open and close pop-up Edit Profile
+export const editBtn = document.querySelector('.profile__edit-bth');
+export const popupProfile = document.querySelector('.popup_action_edit-profile');
+
+// Open and close pop-up Add Place
+export const addPlaceBtn = document.querySelector('.profile__add-btn');
+export const popupPlace = document.querySelector('.popup_action_add-place');
+export const popupPlaceSubmitBtn = popupPlace.querySelector('.popup__save-btn');
+
+// Get info about user
+export const userName = document.querySelector('.profile__name');
+export const userProfession = document.querySelector('.profile__profession');
+export const popupName = popupProfile.querySelector('.popup__input_data_name');
+export const popupProfession = popupProfile.querySelector('.popup__input_data_job');
+
+// Get info about place
+export const popupPlaceName = popupPlace.querySelector('.popup__input_data_place-name');
+export const popupPlaceLink = popupPlace.querySelector('.popup__input_data_place-link');
+
+// Save new user info
+export const formEditUser = popupProfile.querySelector('.popup__form');
+
+// Save new place
+export const formAddPlace = popupPlace.querySelector('.popup__form');
+
+// Places
+export const places = document.querySelector('.elements');
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+export const settings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-btn',
+  inactiveButtonClass: 'popup__save-btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
