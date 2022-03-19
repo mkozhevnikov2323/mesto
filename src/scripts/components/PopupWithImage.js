e3d358a07484e0f2342825ca85592f32a5d88010
Maksim Kozhevnikov <mkozhevnikov23@yandex.ru>
2022-03-20 01:15:46 +0400

    Правки класса PopupWithImage

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,21 +1,16 @@
 import { Popup } from "./Popup.js";
 import {
-  popupShowPlace,
   imageShowPlace,
   titleShowPlace
 } from '../utils/constants.js';
 
 export class PopupWithImage extends Popup {
-  constructor(data, popupSelector) {
-    super(popupSelector);
-    this._name = data.name;
-    this._link = data.link;
-  }
 
-  open() {
-    this._popupSelector.classList.add('popup_opened');
-    imageShowPlace.src = this._link;
-    imageShowPlace.alt = this._name;
-    titleShowPlace.textContent = this._name;
+  open({name, link}) {
+    super.open();
+
+    imageShowPlace.src = link;
+    imageShowPlace.alt = name;
+    titleShowPlace.textContent = name;
   }
 }
